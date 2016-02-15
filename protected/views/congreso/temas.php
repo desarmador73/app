be3d9a04341d8880913f784b164e0d1f4245d5cf
victor <victor@victor-OptiPlex-9020>
2016-02-15 13:37:29 -0600

    PRUEBA DE TABLA contenido congreso

--- protected/views/congreso/temas.php
+++ protected/views/congreso/temas.php
@@ -6,9 +6,16 @@ $this->pageTitle=Yii::app()->name;
 foo original
 
 <div class="container">
-    <h2>
-        hOLA
-    </h2>
-      
-    
+    <h2> Diseño mecánico </h2>
+    <table class="table-bordered"> 
+        <thead>
+            <tr>
+                <th>Análisis y simuación</th>
+                <th>Diseño de maquinaria y equipo industrial</th>
+            </tr>
+                   
+        </thead>
+        
+    </table>
+        
 </div>
