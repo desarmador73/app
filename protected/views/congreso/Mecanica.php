64774a74ac9d777ea74751eb283a7ccc7b7b021d
victor <victor@victor-OptiPlex-9020>
2016-02-15 17:19:45 -0600

    contenido pagina mecanica

--- protected/views/congreso/Mecanica.php
+++ protected/views/congreso/Mecanica.php
@@ -3,3 +3,20 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
+<div class="row">
+    <div class="col-sm-4">
+        <h3>Mecanica teorica</h3>
+        <p></p>
+    </div>
+    <div class="col-sm-4">
+        <h3> </h3>
+        <p>Analisis experimental</p>
+        <p>Diseño de maquinaria y equipo industrial</p>
+        <p>Mecanica de la fractura</p>
+        <p>Modelado</p>
+        <p>Analisis y simulación</p>
+        <p>Rotodinamica y vibraciones</p>
+        
+    </div>
+    
+</div>
