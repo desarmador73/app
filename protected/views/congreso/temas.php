5d9e360ff427e2ffe3035fa5b15861540c46fc04
victor <victor@victor-OptiPlex-9020>
2016-02-19 18:32:35 -0600

    Cambio a una sola pantalla

--- protected/views/congreso/temas.php
+++ protected/views/congreso/temas.php
@@ -9,15 +9,106 @@ $this->pageTitle=Yii::app()->name;
     <table class="table table-bordered"> 
         <thead>
             <tr>
-                <th>Análisis y simuación</th>
+             <th class="h4">Análisis y simuación</th>
+            </tr>
+            <tr>
                 <th>Diseño de maquinaria y equipo industrial</th>
+            </tr>
+            <tr>
                 <th>Robotica</th>
+            </tr>
+            <tr>
                 <th>Manufacura</th>
+            </tr>
+            <tr>
                 <th>Maquinas y herramientas</th>
             </tr>
                    
         </thead>
         
+    </table>
+    <h2> Materiales y Manofactura </h2>
+    <table class="table table-bordered"> 
+        <thead>
+            <tr>
+             <th>Acabados Superficiales</th>
+            </tr>
+            <tr>
+                <th>Análisis y Simulación</th>
+            </tr>
+            <tr>
+                <th>Manufactura y Máquinas Herramientas</th>
+            </tr>
+            <tr>
+                <th>Metodos de Unión</th>
+            </tr>
+            <tr>
+                <th>Equipo Experimental</th>
+            </tr>
+            <tr>
+                <th>Polimeros y Compuestos</th>
+            </tr>        
+        </thead>
+    </table>
+    <h2> Mecánica Teórica </h2>
+    <table class="table table-bordered"> 
+        <thead>
+            <tr>
+             <th>Análisis experimental</th>
+            </tr>
+            <tr>
+                <th>Diseño de Maquinaria y Equipo Industrial</th>
+            </tr>
+            <tr>
+                <th>Mecánica de la Fractura</th>
+            </tr>
+            <tr>
+                <th>Metodos de Unión</th>
+            </tr>
+            <tr>
+                <th>Modelado</th>
+            </tr>
+            <tr>
+                <th>Análisis y Simulación</th>
+            </tr>        
+            <tr>
+                <th>Rotodinámica y Vibraciones</th>
+            </tr>
+        </thead>
+    </table>
+    <h2> Termofluidos </h2>
+    <table class="table table-bordered"> 
+        <thead>
+            <tr>
+             <th>Dinámica de Fluidos</th>
+            </tr>
+            <tr>
+                <th>Energías Alternas</th>
+            </tr>
+            <tr>
+                <th>Modelado</th>
+            </tr>
+            <tr>
+                <th>Análisis y Simulación</th>
+            </tr>
+            <tr>
+                <th>Sistemas Térmicos</th>
+            </tr>
+        </thead>
+    </table>
+    <h2> Educación en Ingeniería Mecánica  </h2>
+    <table class="table table-bordered"> 
+        <thead>
+            <tr>
+             <th>Análisis Curricular y Perfil de Egreso</th>
+            </tr>
+            <tr>
+                <th>Equipo Experimental</th>
+            </tr>
+            <tr>
+                <th>Herramientas Didacticas y Pedagogia</th>
+            </tr>
+        </thead>
     </table>
         
 </div>
