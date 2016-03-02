4569b5da19415448bf93b2e7ae9abd7134ad5f80
victor <victor@victor-OptiPlex-9020>
2016-03-01 19:53:46 -0600

    Pantalla temas iberomat

--- protected/views/congreso/temasiberomat.php
+++ protected/views/congreso/temasiberomat.php
@@ -3,3 +3,84 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
+<div class="container">
+     <h2> Temas IBEROMAT </h2>
+     <table class="table table-bordered">
+         <thead>
+            <tr>
+                <th>S01. Metalurgia extractiva y elaboración</th>
+            </tr>
+            <tr>
+                <th>S02. Fundición, solidificación y soldadura</th>
+            </tr>
+            <tr>
+                <th>S03. Pulvimetalurgia</th>
+            </tr>
+            <tr>
+                <th>S04. Metalurgia física, deformación plástica y propiedades mecánicas</th>
+            </tr>
+            <tr>
+                <th>S05. Tratamientos térmicos y transformaciones de fase</th>
+            </tr>
+            <tr>
+                <th>S06. Corrosión, protecciones y electroquímica</th>
+            </tr>
+            <tr>
+                <th>S07. Películas delgadas y tratamientos de superficie</th>
+            </tr>
+            <tr>
+                <th>S08. Fractura, fatiga y análisis de fallas</th>
+            </tr>
+            <tr>
+                <th>S09. Tribología, superficies y desgaste</th>
+            </tr>
+            <tr>
+                <th>S10. Materiales cerámicos, refractarios y vítreos</th>
+            </tr>
+            <tr>
+                <th>S11. Materiales para la construcción</th>
+            </tr>
+            <tr>
+                <th>S12. Meteriales poliméricos</th>
+            </tr>
+            <tr>
+                <th>S13. Biomateriales</th>
+            </tr>
+            <tr>
+                <th>S14. Meteriales compuestos</th>
+            </tr>
+            <tr>
+                <th>S15. Meteriales nucleares</th>
+            </tr>
+            <tr>
+                <th>S16. Meteriales poliméricos</th>
+            </tr>
+            <tr>
+                <th>S17. Superconductoresy conductores iónicos</th>
+            </tr>
+            <tr>
+                <th>S18. Simulación computacional de materiales</th>
+            </tr>
+            <tr>
+                <th>S19. Caracterización de materiales por métodos ópticos, acústicos y otros</th>
+            </tr>
+             <tr>
+                <th>S20. Metalurgía histórica</th>
+            </tr>
+            <tr>
+                <th>S21. Educación en ciencia y tecnología de materiales</th>
+            </tr>
+            <tr>
+                <th>S22. Transferencia tecnológica</th>
+            </tr>
+            <tr>
+                <th>S23. Control de calidad y temas afines</th>
+            </tr>
+            <tr>
+                <th>S24. Materiales lignocelulósicos</th>
+            </tr>
+         </thead>
+     </table>
+         
+    
+</div>
